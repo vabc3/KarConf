3f5986f381b98a9112604e8153276854044bec09
kar <vabc3h@gmail.com>
2017-03-23 15:39:35 +0800

    split zsh config files.

---
+++ zsh/go.zsh
@@ -0,0 +1,2 @@
+export GOPATH=~/go1
+export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
---
+++ zsh/kube.zsh
@@ -0,0 +1,9 @@
+hash -d kube=~/go1/src/k8s.io/kubernetes
+
+alias k='kubectl'
+alias ko='kubectl -o wide'
+alias kg='ko get'
+alias kp='kg pods'
+alias ks='kg svc'
+alias kc='kubectl exec -ti'
+alias kd='kubectl delete'
