c3c4b28c61c8f0a8c3dfd4d3a2adfc2e235cc40a
kar <vabc3h@gmail.com>
2017-04-27 13:04:08 +0800

    kube update.

--- zsh/kube.zsh
+++ zsh/kube.zsh
@@ -1,10 +1,18 @@
 hash -d kube=~/go1/src/k8s.io/kubernetes
+hash -d k8s.devutil=~/go1/src/karatal.visualstudio.com/k8s.test/_git/k8s.devutil
 
 alias k='kubectl'
-alias ko='kubectl -o wide'
+alias kc='k config'
+alias kcc='kc current-context'
+alias kcu='kc use-context'
+alias ko='k -o wide'
 alias kg='ko get'
 alias kp='kg pods'
 alias ks='kg svc'
-alias kc='kubectl exec -ti'
-alias kd='kubectl delete'
+alias kexe='kubectl exec -ti'
+alias kd='kubectl describe'
+alias ke='kubectl delete'
 alias kv='kubectl version'
+alias kcf='kubectl create -f'
+alias kep='ke pods'
+alias kdp='kd pods'
