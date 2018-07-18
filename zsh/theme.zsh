aec1513a052a3716a880eb39da6ee3430c9cf0ae
vabc3 <vabc3h@gmail.com>
2018-07-18 16:22:49 +0800

    Update zsh

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,8 +1,10 @@
-ZSH_THEME="powerlevel9k/powerlevel9k"
+if ! [[ $TERM =~ 'linux' ]]; then
+  ZSH_THEME="powerlevel9k/powerlevel9k"
+fi
+#POWERLEVEL9K_MODE=awesome-fontconfig
 #POWERLEVEL9K_CONTEXT_TEMPLATE="%m"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv context dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status)
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 
-# [ -n "$TMUX" ] && export TERM=screen-256color
 
