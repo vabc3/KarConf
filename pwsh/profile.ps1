143150610a4dd059f752031271a34fd5aea8e824
vabc3h <vabc3@gmail.com>
2023-12-15 18:31:01 +0800

    Update pwsh

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,30 +1,29 @@
-function gfr() {
-    git fetch --all -p
-    git checkout master
-    git reset --hard origin/master
-}
+# git
+$git_prefix="$env:username" + "/"
 
-function gfp() {
-    git fetch --all -p
-}
+function ga() {git add -A .}
+function gacp() {ga;git commit --fixup HEAD;git push}
+function gb() {git checkout}
+function gcx() {git clean -xfd}
+function gd() {git diff}
+function gfp() {git fetch -p --all}
+function gfr() {gfp;gm1;git reset --hard origin/master}
+function gl() {git log --graph --full-history --all --pretty=format:"%h%x09%d%x20%s"}
+function gm1() {git checkout master}
+function gmm() {git commit --amend --no-edit}
+function gmt() {git commit -m "tmp"}
+function gr() {git reflog}
+function grc() {grh; gcx}
+function grh() {git reset --hard}
+function gs() {git status}
+function gv() {gitk --all}
 
-function grh() {
-    git reset --hard
-}
+function gbn($branch) { git checkout -b $git_prefix$branch }
+function gb($branch) { git checkout $git_prefix$branch }
+function gbd($branch) { git branch -D $git_prefix$branch }
+function gpod($branch) { git push origin :$git_prefix$branch }
 
-function gcx() {
-    git clean -xfd
-}
-
-function gacp() {
-    git add -A . ; git commit --fixup HEAD; git push
-}
-
-function gbn($branch) { git checkout -b $branch }
-function gb($branch) { git checkout $branch }
-function gbd($branch) { git branch -D $branch }
-function gpod($branch) { git push origin :$branch }
-
+# history
 Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
 
